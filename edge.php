fd9373d921b0085e5625e4f484a2256e46f88415
vikash kumar <vikashsinha7795@gmail.com>
2016-03-03 19:14:13 +0530

    Added files via upload

---
+++ edge.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="me.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 40%;
+      margin-top:50px;
+     margin-bottom:80px;
+     margin-right:150px;
+     margin-left:350px;
+  }
+  body {
+      position: relative; 
+  }
+  #section1 {padding-top:50px;height:500px;color: #fff; background-color: #1E88E5;}
+  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
+  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
+  #section41 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
+  #section42 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
+  </style>
+</head>
+<body>
+<div class="container">
+  <div class="jumbotron">
+    <h2>Learning Edge_point </h2> 
+  <p >Web Desiging & Development.</p> 
+   <kbd>Learning is the only key to success...</kbd>
+  </div> 
+</div><div class="pull-left">
+        <li><a href="https://www.facebook.com/profile.php?id=100002052854907"><img src="facebook.jpg"/></a></li>
+       <li><a href="#"><img src="twitter.jpg"/></a></li>
+        <li><a href="#"><img src="google.jpg"/></a></li>
+       <li><a href="#"><img src="linkedin.jpg"/> </a></li>
+     </div>
+   <div class="container">
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#"> <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+       HOME
+        </button>
+</a>
+    </div>
+ 
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="program.html">PROGRAMS</a></li>
+        <li><a href="learningedge.html">WHY LEARNING_EDGE</a></li>
+        <li><a href="#">REFERNCE</a></li> 
+    <li><a href="#">EXAMS</a></li> 
+    <li><a href="#">INTERNSHIP</a></li> 
+    <li><a href="#">NOTICES</a></li> 
+    <li> <a href="#">MEMBERSHIP</a></li> 
+    <li><div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Contact US
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="mailto:vikashsinha7795@gmail.com">Gmail:vikashsinha7795@gmail.com</a></li>
+    <li><a href="https://www.facebook.com/profile.php?id=100002052854907">Facebook:kumar.vikash938@gmail.com</a></li>
+    <li><a href="#">Cell:8860456385</a></li>
+  </ul>
+</div></li>
+      </ul> </div></div></nav></div></div>
+
+     
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="http://www.leoithub.com/images/blog/web-designing-development.jpg" alt="Chania" width="500" height="400">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://www.sololearn.com/Uploads/html-css.jpg" alt="Chania" width="500" height="400">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="http://www.justtotaltech.co.uk/uploads/imported/php-vs-asp1_1740073691.jpg" alt="Flower" width="500" height="400">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2013/07/bootstrap.png" alt="Flower" width="500" height="400">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://images.phpgang.com/2014/09/12-Best-JavaScript-Libraries-for-2014-You-Shouldnt-Miss.jpg" alt="Flower" width="500" height="300">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+  
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<div class="jumbotron">
+    <p class="lead"><pre>Learning Edge_point is the platform for you genious to enhance your learning Desire.
+            Here we come to know about the every element using in the WEB Desiging & Development.
+       Before going further ,I would to like to tell ,be the ready to learn what you are thinking about.</p></pre>
+</div>
+ <div class="row">
+  <div class="col-md-4">
+   <h3><font color=" #f4511e"><b> <u>Mission</u></b></font></h3><br>
+    <p>Learning Edge_point is optimized for learning, testing, and training.<br> Examples might be simplified to improve reading and basic understanding. </p>
+  </div>
+  <div class="col-md-4"> 
+    <h3><font color=" #f4511e"><b><u>Our Work</u></b></font></h3><br>
+    <p>Tutorials, references, and examples are constantly reviewed to avoid errors by our hard work, but we cannot warrant full correctness of all content. </p>
+
+  </div>
+  <div class="col-md-4"> 
+   <h3><font color=" #f4511e"><b><u>Validate</u></b></font></h3><br>
+    <p>Copyright 2015-2020 by Goverment of India.<br> All Rights Reserved.
+.</p><br><br><br><br><br><br>
+  </div>
+</div>
+</body>
+</html>
